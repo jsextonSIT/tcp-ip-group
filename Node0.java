ee6064135d429b9dc8e2853b10be0c12ba449d8e
ichugwindex <noelw55@gmail.com>
2015-04-23 19:20:42 -0400

    Created basic update method

--- Node0.java
+++ Node0.java
@@ -2,6 +2,7 @@ public class Node0 implements Node{
 	private int nodes = 4;
 	private int[][] distance_table = new int[nodes][nodes];
 	private final int INFINITY = 2147483647;
+	private int nodeId = 0;
 	public Node0(){
 		rtinit();
 	}
@@ -18,6 +19,22 @@ public class Node0 implements Node{
 		distance_table[0][2] = 3;
 		distance_table[0][3] = 7;		
 	}
-	public void rtupdate(){
+	public void rtupdate(Packet rcvdpkt){
+		int distCost;
+		//get the sourceid as the id of the node sending the packet
+		int srcId = rcvdpkt.sourceId;
+		int minCost[] = rcvdpkt.minCost;
+		//get the cost of this node to the target - srcId
+		int cost = distance_table[nodeId][srcId];
+		for (int i = 0; i < 4; i++){
+			//get the distance cost from target node to each node
+			distCost = minCost[i];
+			//check if the cost from node to target to another node is less than the cost of node to another node
+			if((distCost + cost) < distance_table[nodeId][i]){
+				//update our distance_table
+				distance_table[nodeId][i] = distCost + cost;
+				//notify neighbor nodes
+			}
+		}
 	}
 }
