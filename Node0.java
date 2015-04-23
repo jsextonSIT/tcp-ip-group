2cdd4c4ab2c1e6c03f2f278a009b6dd3ba0fab19
ichugwindex <noelw55@gmail.com>
2015-04-23 19:03:41 -0400

    Working on update

--- Node0.java
+++ Node0.java
@@ -1,9 +1,15 @@
 public class Node0 implements Node{
 	private int[][] distance_table;
+	final int nodeId = 0;
+	
 	public void rtinit0(){
 			
 	}
-	public void rtupdate(){
-
+	public void rtupdate(Packet rcvdpkt){
+		//get the sourceid as the id of the node sending the packet
+		int srcId = rcvdpkt.sourceId;
+		for (int i = 0; i < 4; i++){
+			
+		}
 	}
 }
